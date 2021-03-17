cdc28aeb103016c4e73d3590ffb3dc0881549ddc
Mateus Antuane <mateus_antuane@unifei.edu.br>
2021-03-17 18:24:26 -0300

    Atualização com o custom field

--- page-home.php
+++ page-home.php
@@ -2,9 +2,9 @@
 	//Template Name: Menu da Semana
 ?>
 <?php get_header(); ?>
-
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<section class="container">
-			<h2 class="subtitulo">Menu da Semana</h2>
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
 
 			<div class="menu-item grid-8">
 				<h2>Peixes</h2>
@@ -61,4 +61,5 @@
 			</div>
 
 		</section>
+	<?php endwhile; else: endif;?>
 <?php get_footer(); ?>
